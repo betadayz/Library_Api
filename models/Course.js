d64538b16d9cdc7289370ad589977f9fdb0e12f5
BeTa_DaYz <betadayz888@gmail.com>
2021-04-13 10:02:17 +0100

    Aggregate with calculation of average course cost

--- models/Course.js
+++ models/Course.js
@@ -48,15 +48,21 @@ CourseSchema.statics.getAverageCost = async function(bootcampId) {
             }
         }
     ]);
-    console.log(obj);
+    try {
+      await this.model('Bootcamp').findByAndUpdate(bootcampId, {
+          averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+      })
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 CourseSchema.post('save', function() {
-
+   this.constructor.getAverageCost(this.bootcamp);
 });
 
 CourseSchema.pre('remove', function() {
-
+   this.constructor.getAverageCost(this.bootcamp)
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
