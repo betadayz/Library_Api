251b2606e636f4537d651db7edae3810c4f425c4
BeTa_DaYz <betadayz888@gmail.com>
2021-04-14 16:37:52 +0100

    Sign get JSON Web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,6 +11,8 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     });
-    
-    res.status(200).json({ success: true });
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
 });
