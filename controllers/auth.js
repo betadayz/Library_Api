64b3de3ad50703dcea793d76346932591926a042
BeTa_DaYz <betadayz888@gmail.com>
2021-04-14 14:28:03 +0100

    Setting up controller and route for authorization

---
+++ controllers/auth.js
@@ -0,0 +1,7 @@
+const ErrorResponse = require('../utilis/errorResponse');
+const asyncHandler = require('../middleware/async');
+const User = require('../models/User');
+
+exports.register = asyncHandler(async (req, res, next) => {
+    res.status(200).json({ success: true });
+});
