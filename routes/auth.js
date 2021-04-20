526a51c97146d93b174deafd6a457a1d534ff7d5
BeTa_DaYz <betadayz888@gmail.com>
2021-04-20 11:34:04 +0100

    Authorization protection middleware completed

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,10 @@ const { register, login, getMe  } = require('../controllers/auth');
 
 const router = express.Router();
 
+const { protect } = require('../middleware/auth');
+
 router.post('/register', register);
 router.get('/login', login);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 module.exports = router;
